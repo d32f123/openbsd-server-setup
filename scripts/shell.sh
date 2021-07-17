66d7b2a9cd081b305701a79f55a219286f12998f
Andrey Nesterov <anesterov@anesterov.xyz>
2021-07-17 23:59:32 +0300

    Fix file permissions

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -4,8 +4,10 @@
 chsh -s "$(which zsh)"
 
 # setup shell
+pushd
 git clone --depth 1 https://github.com/d32f123/shell-environment.git
 ./shell-environment/setup.sh
+popd
 
 source ~/.config/zsh/.zshrc
 
